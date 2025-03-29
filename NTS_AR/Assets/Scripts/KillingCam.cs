fbda5d77bf8b43688ba49aaa3c4a2970a5932ff0
Niko <nikolas.palaiodimopoulos@epita.fr>
2025-03-29 16:17:03 +0100

    niko

---
+++ NTS_AR/Assets/Scripts/KillingCam.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillingCam : MonoBehaviour
+{
+    public GameObject ParticleEffect;
+    private Vector2 touchPos;
+    private RaycastHit hit;
+    private Camera cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
